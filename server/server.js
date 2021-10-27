6596d19bd8be07516be1907c46b93caaaefb9d4a
Eric Humphries <ezhumphries@gmail.com>
2021-10-27 14:09:07 -0700

    removed unnecesaries

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/restaurants/:restaurantId', express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(
+  "/restaurants/:restaurantId",
+  express.static(path.join(__dirname, "../public"))
+);
 
 app.listen(port, () => {
   console.log(`server running at: ${port}`);
